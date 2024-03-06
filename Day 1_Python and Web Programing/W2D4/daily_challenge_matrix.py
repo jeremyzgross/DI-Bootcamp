0d322232a03b82b964392c93f3fa5667b5d1696a
jeremyzgross <jeremyzekegross@gmail.com>
2024-03-06 19:44:58 +0200

    function ex 1-7

---
+++ Day 1_Python and Web Programing/W2D4/daily_challenge_matrix.py
@@ -0,0 +1,25 @@
+
+# This is Matrix!
+
+# Given a Matrix string
+
+matrix_string = ('7iiTsxh%?i #sM $a #t%^r!')
+
+# iterate through the string and make each 3 characters it's own list
+# make each character in every list it's own string
+# it should now look like matrix_2d
+matrix_2d = [["7", 'i', 'i'],
+           ['T', 's', 'x'],
+           ['h', '%', '?'],
+           ['i', ' ', '#'],
+           ['s', 'M', ' '],
+           ['$', 'a', ' '],
+           ['#', 't', '%']
+           ['^', 'r', '!']]
+
+# make a function that will change the empty variable decoded_messege into "This is Matrix"
+
+decoded_messege = ''
+
+
+
