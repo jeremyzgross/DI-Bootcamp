f6a8abedd4862be359a4a98ce7d2c3f008de4766
jeremyzgross <jeremyzekegross@gmail.com>
2024-03-09 13:26:40 +0200

    List Concatenation Excercises XP Gold W2D2

---
+++ Day 1_Python and Web Programing/W2D5/PYTHON REVIEW/EXRCISES_XP_GOLD_W2D2.py
@@ -0,0 +1,9 @@
+list_1 = ['A', 'B']
+list_2 = [1,2]
+
+list_combined = []
+for item in list_1:
+    list_combined.append(item)
+for item in list_2:
+    list_combined.append(item)
+print(list_combined)
