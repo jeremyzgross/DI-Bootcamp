708ec34942d151eb51730662fd75980ce0593215
jeremyzgross <jeremyzekegross@gmail.com>
2024-03-18 15:05:04 +0200

    xp

--- Day 1_Python and Web Programing/W4D1/W4D1_EX_xp.sql
+++ Day 1_Python and Web Programing/W4D1/W4D1_EX_xp.sql
@@ -1,37 +1,37 @@
--- CREATE TABLE items(
--- 	furniture_id SERIAL PRIMARY KEY, 
--- 	item VARCHAR(50) NOT NULL,
--- 	price SMALLINT NOT NULL
+CREATE TABLE items(
+	furniture_id SERIAL PRIMARY KEY, 
+	item VARCHAR(50) NOT NULL,
+	price SMALLINT NOT NULL
 	
--- )
+)
 
 
--- INSERT INTO items (item, price)
--- VALUES
--- ('Small Desk', '100'),
--- ('Large Desk', '300'),
--- ('Fan', '80');
+INSERT INTO items (item, price)
+VALUES
+('Small Desk', '100'),
+('Large Desk', '300'),
+('Fan', '80');
 
--- CREATE TABLE customers(
--- 	customer_id SERIAL PRIMARY KEY, 
--- 	first_name VARCHAR(50) NOT NULL,
--- 	last_name VARCHAR(100) NOT NULL
--- )
+CREATE TABLE customers(
+	customer_id SERIAL PRIMARY KEY, 
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(100) NOT NULL
+)
 
--- INSERT INTO customers (first_name, last_name)
--- VALUES
--- ('Greg', 'Jones'),
--- ('Sandra', 'Jones'),
--- ('Scott', 'Scott'),
--- ('Trevor', 'Green'),
--- ('Melanie', 'Johnson');
+INSERT INTO customers (first_name, last_name)
+VALUES
+('Greg', 'Jones'),
+('Sandra', 'Jones'),
+('Scott', 'Scott'),
+('Trevor', 'Green'),
+('Melanie', 'Johnson');
 
--- SELECT* FROM items
--- SELECT price FROM items WHERE price > 80;
--- SELECT price FROM items WHERE price <= 300;
--- SELECT first_name, last_name FROM customers WHERE last_name = 'Smith';
+SELECT* FROM items
+SELECT price FROM items WHERE price > 80;
+SELECT price FROM items WHERE price <= 300;
+SELECT first_name, last_name FROM customers WHERE last_name = 'Smith';
 --returns an empty set 
--- SELECT first_name, last_name FROM customers WHERE last_name = 'Jones';
--- SELECT first_name, last_name FROM customers WHERE last_name != 'Smith';
+SELECT first_name, last_name FROM customers WHERE last_name = 'Jones';
+SELECT first_name, last_name FROM customers WHERE last_name != 'Smith';
 
 
