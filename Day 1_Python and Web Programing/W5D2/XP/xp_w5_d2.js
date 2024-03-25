a38bbb4d2fa2c65c31155c51cd94d578354e0f92
jeremyzgross <jeremyzekegross@gmail.com>
2024-03-25 16:31:32 +0200

    Started xp

---
+++ Day 1_Python and Web Programing/W5D2/XP/xp_w5_d2.js
@@ -0,0 +1,54 @@
+//1
+
+
+// const people = ["Greg", "Mary", "Devon", "James"];
+// people.shift()
+// console.log(people);
+
+// people.splice(2, 3, 'Jason')
+// console.log(people);
+
+// people.push('Jeremy')
+// console.log(people);
+
+// console.log(people.indexOf('Mary'));
+
+// let peopleSliced = people.slice(1,3)
+// console.log(peopleSliced);
+
+// console.log(people.indexOf('Foo'));
+// //-1 because it does not exist
+
+// let last = people
+// // console.log(last[-1]); //this does not work like in python because js is zero based indexing
+// console.log(last[last.length - 1]);
+
+// //Using a loop, iterate through the people array and console.log each person.
+// for (index = 0; index < people.length; index++){
+//     console.log(people[index]);
+// }
+
+//Using a loop, iterate through the people array and exit the loop after you console.log “Devon” .
+//Hint: take a look at the break statement in the lesson.
+
+// for (index = 0; index <= people.indexOf('Devon'); index++){
+//     console.log(people[index]);
+// }
+
+
+//2
+
+// let colors = ['blue', 'red', 'green', 'pink', 'orange']
+
+// for(let color in colors){
+//     let value = colors[color];
+//     let message = `My # ${color} choice is ${value}`
+//     console.log(message);
+
+// } //I know this is not the right way but I wasnt sure how else to do
+
+// 3
+
+// do {
+//     userInput = prompt('feed me a number greater than 10')
+// }while (parseInt(userInput) < 10)
