2eb8fb94cbca771d272c97bff216ea6fd9926014
jeremyzgross <jeremyzekegross@gmail.com>
2024-04-07 18:01:20 +0300

    daily challenge part 2 partial

--- Day 1_Python and Web Programing/W7D1/DAILY CHALLENGE/daily_challenge_w7d1.js
+++ Day 1_Python and Web Programing/W7D1/DAILY CHALLENGE/daily_challenge_w7d1.js
@@ -1,48 +1,47 @@
-// /*Create two functions. Each function should return a promise.
-// The first function called makeAllCaps(), takes an array of words as an argument
-// If all the words in the array are strings, resolve the promise. The value of the resolved promise is the array of words uppercased.
-// else, reject the promise with a reason.
-// The second function called sortWords(), takes an array of words uppercased as an argument
-// If the array length is bigger than 4, resolve the promise. The value of the resolved promise is the array of words sorted in alphabetical order.
-// else, reject the promise with a reason. */
+// // /*Create two functions. Each function should return a promise.
+// // The first function called makeAllCaps(), takes an array of words as an argument
+// // If all the words in the array are strings, resolve the promise. The value of the resolved promise is the array of words uppercased.
+// // else, reject the promise with a reason.
+// // The second function called sortWords(), takes an array of words uppercased as an argument
+// // If the array length is bigger than 4, resolve the promise. The value of the resolved promise is the array of words sorted in alphabetical order.
+// // else, reject the promise with a reason. */
 
+// // /*function makeAllCaps(arr) {
+// //   return new Promise((resolve, reject) => {
+// //     for (i = 0; i < arr.length; i++) {
+// //       if (typeof arr[i] === 'string') {
+// //         resolve(arr[i].toUpperCase())
+// //       } else {
+// //         reject('not all a string')
+// //       }
+// //     }
+// //   })
+// // }*/
 
-// /*function makeAllCaps(arr) {
+// function makeAllCaps(arr) {
 //   return new Promise((resolve, reject) => {
-//     for (i = 0; i < arr.length; i++) {
+//     const capsArray = []
+//     for (let i = 0; i < arr.length; i++) {
 //       if (typeof arr[i] === 'string') {
-//         resolve(arr[i].toUpperCase())
+//         capsArray.push(arr[i].toUpperCase())
 //       } else {
-//         reject('not all a string')
+//         reject('not all strings')
+//         return
 //       }
 //     }
+//     resolve(capsArray)
 //   })
-// }*/
+// }
 
-function makeAllCaps(arr) {
-  return new Promise((resolve, reject) => {
-    const capsArray = []
-    for (let i = 0; i < arr.length; i++) {
-      if (typeof arr[i] === 'string') {
-        capsArray.push(arr[i].toUpperCase())
-      } else {
-        reject('not all strings')
-        return
-      }
-    }
-    resolve(capsArray)
-  })
-}
-
-function sortWords(arr) {
-  return new Promise((resolve, reject) => {
-    if (arr.length >= 4) {
-      resolve(arr.sort())
-    } else {
-      reject('arr too short')
-    }
-  })
-}
+// function sortWords(arr) {
+//   return new Promise((resolve, reject) => {
+//     if (arr.length >= 4) {
+//       resolve(arr.sort())
+//     } else {
+//       reject('arr too short')
+//     }
+//   })
+// }
 
 // // //in this example, the catch method is executed
 // makeAllCaps([1, 'pear', 'banana'])
@@ -62,10 +61,8 @@ function sortWords(arr) {
 //   .then((result) => console.log(result)) //["APPLE","BANANA", "KIWI", "MELON", "PEAR"]
 //   .catch((error) => console.log(error))
 
-
 //part 2
-
-  const morse = `{
+const morse = `{
   "0": "-----",
   "1": ".----",
   "2": "..---",
@@ -112,3 +109,31 @@ function sortWords(arr) {
   "(": "-.--.",
   ")": "-.--.-"
 }`
+
+function toJS(morse) {
+  return new Promise((resolve, reject) => {
+    if (morse.length === 0) {
+      reject('morse string is empty')
+    } else {
+      const morseObject = JSON.parse(morse)
+      resolve(morseObject)
+    }
+  })
+}
+
+function toMorse(morseJS) {
+  const word = prompt('Feed me a word: ')
+  return new Promise((resolve, reject) => {
+    const availibleLetters = Object.keys(morseJS)
+    if (word.split('').some((letter) => !availibleLetters.includes(letter))){
+      reject('please type lowercase')
+    }else{
+      resolve('Thank you')
+    }
+  })
+}
+
+toJS(morse)
+  .then((morseObject) => toMorse(morseObject))
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error))
