9ed3c2c95653f489cc6011e4fbacf6eec3d8fb19
jeremyzgross <jeremyzekegross@gmail.com>
2024-04-25 12:05:37 +0300

    w8d3 exercise xp1

--- Day 1_Python and Web Programing/W8D3/EXERCISE XP/EX 1/Controllers/blog.controller.js
+++ Day 1_Python and Web Programing/W8D3/EXERCISE XP/EX 1/Controllers/blog.controller.js
@@ -1,6 +1,12 @@
 //controler
 
-const { _getAllPosts } = require('../Models/blog.model.js')
+const {
+  _getAllPosts,
+  _getPost,
+  _createPost,
+  _updatePost,
+  _deletePost,
+} = require('../Models/blog.model.js')
 
 const getAllPosts = (req, res) => {
   _getAllPosts()
@@ -13,6 +19,54 @@ const getAllPosts = (req, res) => {
     })
 }
 
+const getPost = async (req, res) => {
+  try {
+    const postId = req.params.id
+    const result = await _getPost(postId)
+    res.json(result)
+  } catch (error) {
+    console.error('Error getting post with ID', error)
+    next(error)
+  }
+}
+
+const createNewPost = async (req, res) => {
+  try {
+    const postData = req.body
+    const result = await _createPost(postData)
+    res.json(result)
+  } catch (error) {
+    console.error('Error creating new post', error)
+    next(error)
+  }
+}
+
+const updatePost = async (req, res) => {
+  try {
+    const postId = req.params.id
+    const postData = req.body
+    const result = await _updatePost(postData, postId)
+    res.json(result)
+  } catch (error) {
+    console.error('Error updating new post', error)
+    next(error)
+  }
+}
+
+const deletePost = async (req, res) => {
+  try {
+    const postId = req.params.id
+    const result = await _deletePost(postId)
+    res.json(`ID number ${postId} post was deleted`)
+  } catch (error) {
+    console.error('Error deleting post with ID', error)
+    next(error)
+  }
+}
 module.exports = {
   getAllPosts,
+  getPost,
+  createNewPost,
+  updatePost,
+  deletePost,
 }
--- Day 1_Python and Web Programing/W8D3/EXERCISE XP/EX 1/Models/blog.model.js
+++ Day 1_Python and Web Programing/W8D3/EXERCISE XP/EX 1/Models/blog.model.js
@@ -11,6 +11,50 @@ const _getAllPosts = async () => {
   }
 }
 
+const _getPost = async (postId) => {
+  try {
+    return await db.select('*').from('posts').where('id', postId)
+  } catch (error) {
+    console.error('Error fetching all posts:', error)
+    throw error
+  }
+}
+const _createPost = async (postData) => {
+  const { title, the_content } = postData
+
+  try {
+    return await db('posts').insert({ title, the_content }, ['id'])
+  } catch (error) {
+    console.error('Error fetching adding post:', error)
+    throw error
+  }
+}
+
+const _updatePost = async (postData, postId) => {
+  const { title, the_content } = postData
+  try {
+    return await db('posts')
+      .update({ title, the_content }, ['id', 'the_content', 'title'])
+      .where('id', postId)
+  } catch (error) {
+    console.error('Error fetching adding post:', error)
+    throw error
+  }
+}
+
+const _deletePost = async (postId) => {
+  try {
+    return await db.delete('*').from('posts').where('id', postId)
+  } catch (error) {
+    console.error('Error delete posts:', error)
+    throw error
+  }
+}
+
 module.exports = {
   _getAllPosts,
+  _getPost,
+  _createPost,
+  _updatePost,
+  _deletePost,
 }
