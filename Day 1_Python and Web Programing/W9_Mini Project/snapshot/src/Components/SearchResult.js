c2b92d8b5996eb4ede01f27aba8e8304ea6e4974
jeremyzgross <jeremyzekegross@gmail.com>
2024-05-11 13:05:07 +0300

    w9d4 daily challenge

--- Day 1_Python and Web Programing/W9_Mini Project/snapshot/src/Components/SearchResult.js
+++ Day 1_Python and Web Programing/W9_Mini Project/snapshot/src/Components/SearchResult.js
@@ -3,13 +3,13 @@ const SearchResult = () => {
   return (
     <>
       <h3 className="img-catagory">INSERT PARAMS SEARCH</h3>
-      <div className="img-div"> 
+      <div className="img-div">
         {/* {images.map((imageUrl, index) => (
           <div className="inner-img-div" key={index}>
             <img src={imageUrl} alt="" />
           </div>
         ))} */}
-      </div> 
+      </div>
     </>
   )
 }
